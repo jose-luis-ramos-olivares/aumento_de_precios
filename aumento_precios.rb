166b0e4e70e5c160cc19bdf15fa24826d61a5701
jose <j.ramos.olivares@gmail.com>
2020-07-06 20:44:23 -0400

    DONE!

--- aumento_precios.rb
+++ aumento_precios.rb
@@ -4,7 +4,7 @@ def augment(array, increase=1.2)
     prices.each do |price|
         new_prices.push (price*increase)
     end
-    print new_prices
+    return new_prices
 end
 
-augment([120, 210, 309, 104, 192])
+print augment([120, 210, 309, 104, 192])
